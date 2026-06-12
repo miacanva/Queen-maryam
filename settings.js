f6016f487bc90def2f60feadd7006d3559055305
ELICTHXE <sultanseifu4@gmail.com>
2026-06-13 00:24:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
+  packname: 'qυєєη мαяуαм',
   author: 'supreme',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
+  botName: "qυєєη мαяуαм",
   botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  ownerNumber: '255623553450', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
